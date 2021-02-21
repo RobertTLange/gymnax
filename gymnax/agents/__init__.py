707e5c984dceca71562b78c4e8d0e84177dc9bf0
Robert Lange <robert.lange@barcelonagse.eu>
2021-02-21 13:03:20 +0100

    Minimal agents for evaluation/interleaved rollouts

---
+++ gymnax/agents/__init__.py
@@ -0,0 +1,6 @@
+from gymnax.agents.minimal import (MinimalEvaluationAgent,
+                                   MinimalInterleavedAgent)
+
+
+__all__ = ["MinimalEvaluationAgent",
+           "MinimalInterleavedAgent"]
---
+++ gymnax/agents/minimal.py
@@ -0,0 +1,43 @@
+class MinimalEvaluationAgent():
+    """ A Minimal Wrapper for an evaluation agent. """
+    def __init__(self, policy):
+        """ Init all key features of the agent. E.g. this may include:
+            - Policy function network forward function (assumes key input)
+            - Exploitation schedule to use in evaluation
+            - Here: Deterministic Agent - but could also be stochastic!
+        """
+        self.policy = policy
+
+    def actor_step(self, key, agent_params, obs, actor_state):
+        """ Policy forward pass + return action and new state. """
+        action = self.policy(key, agent_params, obs)
+        return action, actor_state
+
+    def init_actor_state(self):
+        return None
+
+
+class MinimalInterleavedAgent():
+    """ A Minimal Wrapper for an agent that interleaves steps with updates. """
+    def __init__(self, policy):
+        """ Init all key features of the agent. E.g. this may include:
+            - Policy/Value function network forward function
+            - Optimizer to use in learner_step = Use optax!
+            - Exploration schedule to use in actor_step
+        """
+        self.policy = policy
+
+    def actor_step(self, key, agent_params, obs, actor_state):
+        """ Policy forward pass + return action and new state. """
+        action = self.policy(key, agent_params, obs)
+        return action, actor_state
+
+    def learner_step(self, key, agent_params, learner_state):
+        """ Update the network params + return new state (e.g. of opt). """
+        return agent_params, learner_state
+
+    def init_learner_state(self, agent_params):
+        return None
+
+    def init_actor_state(self):
+        return None
