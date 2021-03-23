31c149bf5d045f045d847d6fbd8cc57a9ea85a30
Robert Lange <robert.lange@barcelonagse.eu>
2021-03-23 10:09:57 +0100

    Freeway - `get_obs`

--- gymnax/environments/minatar/freeway.py
+++ gymnax/environments/minatar/freeway.py
@@ -44,13 +44,12 @@ def reset(rng_input, params):
 
 def get_obs(state):
     """ Return observation from raw state trafo. """
-    obs = jnp.zeros((10, 10, 6), dtype=bool)
+    obs = jnp.zeros((10, 10, 7), dtype=bool)
     # Set the position of the chicken agent, cars, and trails
     obs = jax.ops.index_update(obs, jax.ops.index[state["pos"], 4, 0], 1)
     for car_id in range(8):
         car = state["cars"][car_id]
-        obs = jax.ops.index_update(obs, jax.ops.index[state["pos"],
-                                                      car[1], car[0], 1], 1)
+        obs = jax.ops.index_update(obs, jax.ops.index[car[1], car[0], 1], 1)
         # Boundary conditions for cars
         back_x = ((car[3] > 0) * (car[0] - 1) +
                   (1 - (car[3] > 0)) * (car[0] + 1))
@@ -58,18 +57,14 @@ def get_obs(state):
         right_out = (back_x > 9)
         back_x = left_out * 9 + (1 - left_out) * back_x
         back_x = right_out * 0 + (1 - right_out) * back_x
-        # TODO: Continue working on state to obs!
-        # if(abs(car[3])==1):
-        #     trail = self.channels['speed1']
-        # elif(abs(car[3])==2):
-        #     trail = self.channels['speed2']
-        # elif(abs(car[3])==3):
-        #     trail = self.channels['speed3']
-        # elif(abs(car[3])==4):
-        #     trail = self.channels['speed4']
-        # elif(abs(car[3])==5):
-        #     trail = self.channels['speed5']
-        # state[car[1],back_x, trail] = 1
+        # Set trail to be on
+        trail_channel = (2 * (jnp.abs(car[3]) == 1) +
+                         3 * (jnp.abs(car[3]) == 2) +
+                         4 * (jnp.abs(car[3]) == 3) +
+                         5 * (jnp.abs(car[3]) == 4) +
+                         6 * (jnp.abs(car[3]) == 5))
+        obs = jax.ops.index_update(obs, jax.ops.index[car[1], back_x,
+                                                      trail_channel], 1)
     return obs
 
 
