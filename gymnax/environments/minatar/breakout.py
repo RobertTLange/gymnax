6c728906b3b0dcffd369f0ad1458885f6e90b320
Robert Lange <robert.lange@barcelonagse.eu>
2021-04-27 09:21:56 +0200

    `Breakout-MinAtar` passes test

--- gymnax/environments/minatar/breakout.py
+++ gymnax/environments/minatar/breakout.py
@@ -39,87 +39,8 @@ class MinBreakout(environment.Environment):
     def step(self, key: PRNGKey, state: dict, action: int
              ) -> Tuple[Array, dict, float, bool, dict]:
         """ Perform single timestep state transition. """
-        reward = 0
-        state["pos"] = (jnp.maximum(0, state["pos"] - 1) * (action == 1)
-                        + jnp.minimum(9, state["pos"] + 1) * (action == 2)
-                        + state["pos"] * jnp.logical_and(action != 1,
-                                                         action != 2))
-
-        # Update ball position
-        state["last_x"] = state["ball_x"]
-        state["last_y"] = state["ball_y"]
-        new_x = ((state["ball_x"] - 1) * (state["ball_dir"] == 0) +
-                 (state["ball_x"] + 1) * (state["ball_dir"] == 1) +
-                 (state["ball_x"] + 1) * (state["ball_dir"] == 2) +
-                 (state["ball_x"] - 1) * (state["ball_dir"] == 3))
-        new_y = ((state["ball_y"] - 1) * (state["ball_dir"] == 0) +
-                 (state["ball_y"] - 1) * (state["ball_dir"] == 1) +
-                 (state["ball_y"] + 1) * (state["ball_dir"] == 2) +
-                 (state["ball_y"] + 1) * (state["ball_dir"] == 3))
-
-        # Boundary conditions and brick map update
-        strike_toggle = 0
-        border_cond_x = jnp.logical_or(new_x < 0, new_x > 9)
-        new_x = (border_cond_x * (0 * (new_x < 0) + 9 * (new_x > 9))
-                 + (1 - border_cond_x) * new_x)
-        # Reflect ball direction if bounced off at x border
-        state["ball_dir"] = ((border_cond_x *
-                             jnp.array([1, 0, 3, 2])[state["ball_dir"]])
-                             + (1 - border_cond_x) * state["ball_dir"])
-
-        # Reflect ball direction if bounced off at y border
-        border_cond1_y = (new_y < 0)
-        new_y = border_cond1_y * 0 + (1 - border_cond1_y) * new_y
-        state["ball_dir"] = ((border_cond1_y *
-                             jnp.array([3, 2, 1, 0])[state["ball_dir"]])
-                             + (1 - border_cond1_y) * state["ball_dir"])
-
-        # 1st NASTY ELIF BEGINS HERE...
-        strike_toggle = jnp.logical_and(1 - border_cond1_y,
-                                        state["brick_map"][new_y, new_x] == 1)
-        strike_bool = jnp.logical_and((1 - state["strike"]), strike_toggle)
-
-        reward += strike_bool
-        state["strike"] = strike_bool
-        state["brick_map"] = (strike_bool *
-                              jax.ops.index_update(state["brick_map"],
-                                                   new_y, new_x, 0)
-                              + (1 - strike_bool) * state["brick_map"])
-        new_y = (strike_bool * state["last_y"] + (1 - strike_bool) * new_y)
-
-        state["ball_dir"] = (strike_bool * jnp.array([3,2,1,0])[state["ball_dir"]]
-                             + (1 - strike_bool) * state["ball_dir"])
-
-        # 2nd NASTY ELIF BEGINS HERE...
-        new_bricks = jnp.logical_and(1 - strike_toggle, new_y == 9)
-        spawn_bricks = jnp.logical_and(new_bricks,
-                                       jnp.count_nonzero(state["brick_map"]) == 0)
-        state["brick_map"] = (spawn_bricks *
-                              jax.ops.index_update(state["brick_map"],
-                                                   jax.ops.index[1:4, :], 1)
-                              + (1 - spawn_bricks) * state["brick_map"])
-
-        redirect_ball1 = jnp.logical_and(new_bricks,
-                                         state["ball_x"] == state["pos"])
-        state["ball_dir"] = (redirect_ball1 *
-                             jnp.array([3, 2, 1, 0])[state["ball_dir"]]
-                             + (1 - redirect_ball1) * state["ball_dir"])
-        new_y = redirect_ball1 * state["last_y"] + (1 - redirect_ball1) * new_y
-
-        redirect_ball2a = jnp.logical_and(new_bricks, 1 - redirect_ball1)
-        redirect_ball2 = jnp.logical_and(redirect_ball2a, new_x == state["pos"])
-        state["ball_dir"] = (redirect_ball2 *
-                             jnp.array([2, 3, 0, 1])[state["ball_dir"]]
-                             + (1 - redirect_ball2) * state["ball_dir"])
-        new_y = redirect_ball2 * state["last_y"] + (1 - redirect_ball2) * new_y
-        redirect_cond = jnp.logical_and(1 - redirect_ball1, 1 - redirect_ball2)
-        terminal_cond = jnp.logical_and(new_bricks, redirect_cond)
-        state["terminal"] = terminal_cond
-
-        state["strike"] = (0 * (1 - strike_toggle) +
-                           state["strike"] * strike_toggle)
-        state["ball_x"] = new_x
-        state["ball_y"] = new_y
+        state, new_x, new_y = step_agent(state, action)
+        state, reward = step_ball_brick(state, new_x, new_y)
 
         # Check game condition & no. steps for termination condition
         state["time"] += 1
@@ -169,7 +90,7 @@ class MinBreakout(environment.Environment):
     def is_terminal(self, state: dict) -> bool:
         """ Check whether state is terminal. """
         done_steps = (state["time"] > self.env_params["max_steps_in_episode"])
-        return False
+        return jnp.logical_or(done_steps, state["terminal"])
 
     @property
     def name(self) -> str:
@@ -200,3 +121,92 @@ class MinBreakout(environment.Environment):
              "last_x": spaces.Discrete(10),
              "time": spaces.Discrete(self.env_params["max_steps_in_episode"]),
              "terminal": spaces.Discrete(2)})
+
+
+def step_agent(state: dict, action:int) -> Tuple[dict, int, int]:
+    """ Helper that steps the agent and checks boundary conditions. """
+    state["pos"] = (jnp.maximum(0, state["pos"] - 1) * (action == 1)
+                    + jnp.minimum(9, state["pos"] + 1) * (action == 2)
+                    + state["pos"] * jnp.logical_and(action != 1,
+                                                     action != 2))
+
+    # Update ball position
+    state["last_x"] = state["ball_x"]
+    state["last_y"] = state["ball_y"]
+    new_x = ((state["ball_x"] - 1) * (state["ball_dir"] == 0) +
+             (state["ball_x"] + 1) * (state["ball_dir"] == 1) +
+             (state["ball_x"] + 1) * (state["ball_dir"] == 2) +
+             (state["ball_x"] - 1) * (state["ball_dir"] == 3))
+    new_y = ((state["ball_y"] - 1) * (state["ball_dir"] == 0) +
+             (state["ball_y"] - 1) * (state["ball_dir"] == 1) +
+             (state["ball_y"] + 1) * (state["ball_dir"] == 2) +
+             (state["ball_y"] + 1) * (state["ball_dir"] == 3))
+
+    # Boundary conditions and brick map update
+    border_cond_x = jnp.logical_or(new_x < 0, new_x > 9)
+    new_x = (border_cond_x * (0 * (new_x < 0) + 9 * (new_x > 9))
+             + (1 - border_cond_x) * new_x)
+    # Reflect ball direction if bounced off at x border
+    state["ball_dir"] = ((border_cond_x *
+                         jnp.array([1, 0, 3, 2])[state["ball_dir"]])
+                         + (1 - border_cond_x) * state["ball_dir"])
+    return state, new_x, new_y
+
+
+def step_ball_brick(state: dict, new_x: int, new_y: int) -> Tuple[dict, int]:
+    """ Helper that computes reward and termination cond. from brickmap. """
+    reward = 0
+    strike_toggle = 0
+    # Reflect ball direction if bounced off at y border
+    border_cond1_y = (new_y < 0)
+    new_y = lax.select(border_cond1_y, 0, new_y)
+    state["ball_dir"] = lax.select(border_cond1_y,
+                         jnp.array([3, 2, 1, 0])[state["ball_dir"]],
+                         state["ball_dir"])
+
+    # 1st NASTY ELIF BEGINS HERE...
+    strike_toggle = jnp.logical_and(1 - border_cond1_y,
+                                    state["brick_map"][new_y, new_x] == 1)
+    strike_bool = jnp.logical_and((1 - state["strike"]), strike_toggle)
+
+    reward += strike_bool
+    state["strike"] = strike_bool
+    state["brick_map"] = (strike_bool *
+                          jax.ops.index_update(state["brick_map"],
+                                               new_y, new_x, 0)
+                          + (1 - strike_bool) * state["brick_map"])
+    new_y = (strike_bool * state["last_y"] + (1 - strike_bool) * new_y)
+
+    state["ball_dir"] = (strike_bool * jnp.array([3,2,1,0])[state["ball_dir"]]
+                         + (1 - strike_bool) * state["ball_dir"])
+
+    # 2nd NASTY ELIF BEGINS HERE...
+    new_bricks = jnp.logical_and(1 - strike_toggle, new_y == 9)
+    spawn_bricks = jnp.logical_and(new_bricks,
+                                   jnp.count_nonzero(state["brick_map"]) == 0)
+    state["brick_map"] = (spawn_bricks *
+                          jax.ops.index_update(state["brick_map"],
+                                               jax.ops.index[1:4, :], 1)
+                          + (1 - spawn_bricks) * state["brick_map"])
+
+    redirect_ball1 = jnp.logical_and(new_bricks,
+                                     state["ball_x"] == state["pos"])
+    state["ball_dir"] = (redirect_ball1 *
+                         jnp.array([3, 2, 1, 0])[state["ball_dir"]]
+                         + (1 - redirect_ball1) * state["ball_dir"])
+    new_y = redirect_ball1 * state["last_y"] + (1 - redirect_ball1) * new_y
+
+    redirect_ball2a = jnp.logical_and(new_bricks, 1 - redirect_ball1)
+    redirect_ball2 = jnp.logical_and(redirect_ball2a, new_x == state["pos"])
+    state["ball_dir"] = (redirect_ball2 *
+                         jnp.array([2, 3, 0, 1])[state["ball_dir"]]
+                         + (1 - redirect_ball2) * state["ball_dir"])
+    new_y = redirect_ball2 * state["last_y"] + (1 - redirect_ball2) * new_y
+    redirect_cond = jnp.logical_and(1 - redirect_ball1, 1 - redirect_ball2)
+    terminal_cond = jnp.logical_and(new_bricks, redirect_cond)
+    state["strike"] = (0 * (1 - strike_toggle) +
+                       state["strike"] * strike_toggle)
+    state["ball_x"] = new_x
+    state["ball_y"] = new_y
+    state["terminal"] = terminal_cond
+    return state, reward
--- tests/minatar/test_breakout.py
+++ tests/minatar/test_breakout.py
@@ -6,10 +6,18 @@ from gymnax.utils import (np_state_to_jax,
                           assert_correct_transit,
                           assert_correct_state)
 
+from gymnax.environments.minatar.breakout import (step_agent,
+                                                  step_ball_brick)
+
+import numpy as np
+from minatar import Environment
+from breakout_helpers import (step_agent_numpy,
+                              step_ball_brick_numpy)
+
 import numpy as np
 from minatar import Environment
 
-num_episodes, num_steps, tolerance = 2, 10, 1e-04
+num_episodes, num_steps, tolerance = 10, 100, 1e-04
 env_name_gym, env_name_jax = 'breakout', 'Breakout-MinAtar'
 
 
@@ -46,6 +54,35 @@ def test_step():
                                  tolerance)
 
             if done_gym:
+                break
+
+
+def test_sub_steps():
+    """ Test a step transition for the env. """
+    env_gym = Environment(env_name_gym, sticky_action_prob=0.0)
+    rng, env_jax = gymnax.make(env_name_jax)
+
+    # Loop over test episodes
+    for ep in range(num_episodes):
+        obs = env_gym.reset()
+        # Loop over test episode steps
+        for s in range(num_steps):
+            rng, key_step, key_action = jax.random.split(rng, 3)
+            state = np_state_to_jax(env_gym, env_name_jax)
+            action = env_jax.action_space.sample(key_action)
+            action_gym = minatar_action_map(action, env_name_jax)
+
+            new_x, new_y = step_agent_numpy(env_gym, action_gym)
+            state_jax_a, new_x_jax, new_y_jax = step_agent(state, action)
+            assert (new_x == new_x_jax and new_y == new_y_jax)
+            assert_correct_state(env_gym, env_name_jax, state_jax_a,
+                                 tolerance)
+
+            r, term = step_ball_brick_numpy(env_gym, new_x, new_y)
+            state_jax_b, reward = step_ball_brick(state_jax_a, new_x, new_y)
+            assert_correct_state(env_gym, env_name_jax, state_jax_b,
+                                 tolerance)
+            if env_gym.env.terminal:
                 break
 
 
