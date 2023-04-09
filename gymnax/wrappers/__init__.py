ef3f073c55c284de92e832337662b4d8baf83601
RobertTLange <robertlange0@gmail.com>
2023-04-09 19:26:12 +0200

    Fix wrapper import dependency bug

--- gymnax/wrappers/__init__.py
+++ gymnax/wrappers/__init__.py
@@ -1,11 +1,9 @@
 from .dm_env import GymnaxToDmEnvWrapper
-from .evojax import GymnaxToEvoJaxTask
 from .gym import GymnaxToGymWrapper, GymnaxToVectorGymWrapper
 from .purerl import FlattenObservationWrapper, LogWrapper
 
 __all__ = [
     "GymnaxToDmEnvWrapper",
-    "GymnaxToEvoJaxTask",
     "GymnaxToGymWrapper",
     "GymnaxToVectorGymWrapper",
     "FlattenObservationWrapper",
