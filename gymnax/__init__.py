94f7b45f9f5fcd8c4b233702e924c35a2a586601
Aidandos <timon.willi@gmail.com>
2024-04-03 14:16:46 +0000

    gymnax changes

--- gymnax/__init__.py
+++ gymnax/__init__.py
@@ -1,4 +1,12 @@
-from .registration import make, registered_envs
-from .environments import EnvState, EnvParams
+"""Gymnax: A library for creating and registering Gym environments."""
+
+from gymnax import environments
+from gymnax import registration
+
+EnvParams = environments.EnvParams
+EnvState = environments.EnvState
+make = registration.make
+registered_envs = registration.registered_envs
+
 
 __all__ = ["make", "registered_envs", "EnvState", "EnvParams"]
