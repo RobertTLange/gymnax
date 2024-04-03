a8f4480fed28fbd233c9696f5812889ab04a06cb
Aidandos <timon.willi@gmail.com>
2024-04-03 14:17:43 +0000

    minatar changes

--- gymnax/environments/minatar/__init__.py
+++ gymnax/environments/minatar/__init__.py
@@ -1,14 +1,24 @@
-from .asterix import MinAsterix
-from .breakout import MinBreakout
-from .freeway import MinFreeway
-from .seaquest import MinSeaquest
-from .space_invaders import MinSpaceInvaders
+"""Minatar environments."""
+
+from gymnax.environments.minatar import asterix
+from gymnax.environments.minatar import breakout
+from gymnax.environments.minatar import freeway
+from gymnax.environments.minatar import space_invaders
+
+# from gymnax.environments.minatar import seaquest
+
+
+MinAsterix = asterix.MinAsterix
+MinBreakout = breakout.MinBreakout
+MinFreeway = freeway.MinFreeway
+# MinSeaquest = seaquest.MinSeaquest
+MinSpaceInvaders = space_invaders.MinSpaceInvaders
 
 
 __all__ = [
     "MinAsterix",
     "MinBreakout",
     "MinFreeway",
-    "MinSeaquest",
+    # "MinSeaquest",
     "MinSpaceInvaders",
 ]
