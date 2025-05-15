ad1dbe5fb6b41c09173111cc97a534ea3550fff4
Maxence Faldor <maxencefaldor@gmail.com>
2025-05-15 17:29:21 +0000

    Convert jax.tree_util.* to jax.tree.*

--- gymnax/wrappers/brax.py
+++ gymnax/wrappers/brax.py
@@ -73,13 +73,13 @@ class GymnaxToBraxWrapper(envs.Env):
         """DEFAULT size of action vector expected by step."""
         a_space = self.env.action_space(self.env.default_params)
         example_a = a_space.sample(jax.random.key(0))
-        return len(jax.tree.util.tree_flatten(example_a)[0])
+        return len(jax.tree.flatten(example_a)[0])
 
     def observation_size(self) -> int:
         """DEFAULT size of observation vector expected by step."""
         o_space = self.env.observation_space(self.env.default_params)
         example_o = o_space.sample(jax.random.key(0))
-        return len(jax.tree.util.tree_flatten(example_o)[0])
+        return len(jax.tree.flatten(example_o)[0])
 
     def backend(self) -> str:
         """Return backend of the environment."""
