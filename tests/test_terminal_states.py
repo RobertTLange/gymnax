95c6f6bca752d9e1261cb197e092f2dc90677209
YannBerthelot <yannberthelot1@gmail.com>
2025-09-25 01:06:58 +0200

    fixed test

--- tests/test_terminal_states.py
+++ tests/test_terminal_states.py
@@ -5,6 +5,17 @@ import pytest
 import gymnax
 
 
+def split_env_state(state):
+    # stack into list of trees (length = number of timesteps)
+    return [jax.tree.map(lambda x: x[i], state) for i in range(len(state.time))]
+
+
+def split_state_dict(state: dict):
+    keys = state.keys()
+    arrays = state.values()
+    return [{k: v for k, v in zip(keys, values)} for values in zip(*arrays)]
+
+
 @pytest.mark.parametrize(
     "env_id",
     [
@@ -21,28 +32,37 @@ def test_truncation(env_id):
 
     def step_fn(state, _):
         next_obs, next_state, _, done, info = env.step(key, state, action, env_params)
-        return next_state, (next_obs, done, info)
+        return next_state, (next_obs, next_state, done, info)
 
-    _, (observations, dones, infos) = jax.lax.scan(
+    _, (observations, states, dones, infos) = jax.lax.scan(
         f=step_fn, init=state, xs=None, length=env_params.max_steps_in_episode + 1
     )
 
     # Should have at least finished once due to truncation
     assert sum(dones) >= 1
-    for i, (obs, done, info) in enumerate(zip(observations, dones, infos)):
-        if state.time == env_params.max_steps_in_episode - 1:
-            if i + 1 <= len(observations):
-                # Should have truncated
-                assert infos[i + 1]["truncated"]
-                assert not infos[i + 1]["terminated"]
-                assert dones[i + 1]
-                # Last obs from finished episode should be different from \\
-                # first obs of new episode
-                assert not jnp.array_equal(infos[i + 1]["obs_st"], obs)
+    infos = split_state_dict(infos)
+    states = split_env_state(states)
+    for i, (obs, state, done, info) in enumerate(
+        zip(observations, states, dones, infos)
+    ):
+        if i == 0:
+            # Need to observe the step before, not possible at i=0.
+            continue
+        if states[i - 1].time == env_params.max_steps_in_episode - 1:
+            # Should have truncated
+            assert info["truncated"]
+            assert not info["terminated"]
+            assert done
+            # Last obs from finished episode should be different from \\
+            # first obs of new episode
+            assert not jnp.array_equal(info["obs_st"], obs)
+        elif done:
+            assert info["terminated"]
+            assert not info["truncated"]
+            # Last obs from finished episode should be different from \\
+            # first obs of new episode
+            assert not jnp.array_equal(info["obs_st"], obs)
         else:
-            if done:
-                assert info["terminated"]
-                assert not info["truncated"]
-                # Last obs from finished episode should be different from \\
-                # first obs of new episode
-                assert not jnp.array_equal(infos[i + 1]["obs_st"], obs)
+            assert not info["truncated"]
+            assert not info["terminated"]
+            assert jnp.array_equal(info["obs_st"], obs)
